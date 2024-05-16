8e2036000b54b6487d2ca58aa7caf694aa2568ab
Vinicius Passari <vinicius.santos@ataservice.com.br>
2024-05-16 18:38:29 -0300

    Desafio_concluido

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US).useDelimiter("\n");
+
+        System.out.println("Por favor, digite o número da conta!");
+        int numero = scanner.nextInt();
+
+
+        System.out.println("Por favor, digite o número da agência!");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o seu nome!");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o seu saldo!");
+        float saldo = scanner.nextFloat();
+
+        System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo
++ " já está disponivel para saque.");
+
+
+
+
     }
 }
